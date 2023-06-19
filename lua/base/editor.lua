8bb5f4a28143dfcab4845530c83e12c3aa660f07
Nick Revenko <koodeau@users.noreply.github.com>
2023-06-19 16:37:30 +0300

    init

---
+++ lua/base/editor.lua
@@ -0,0 +1,78 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    cmd = "Telescope",
+    -- stylua: ignore
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Git Files" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
+    },
+    opts = {
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-j>"] = function(...)
+              require("telescope.actions").move_selection_next(...)
+            end,
+            ["<C-k>"] = function(...)
+              require("telescope.actions").move_selection_previous(...)
+            end,
+            ["<C-n>"] = function(...)
+              require("telescope.actions").cycle_history_next(...)
+            end,
+            ["<C-p>"] = function(...)
+              require("telescope.actions").cycle_history_prev(...)
+            end,
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      local telescope = require "telescope"
+      telescope.setup(opts)
+      telescope.load_extension "fzf"
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      setup = {
+        show_help = true,
+        plugins = { spelling = true },
+        key_labels = { ["<leader>"] = "SPC" },
+        triggers = "auto",
+        window = {
+          border = "single", -- none, single, double, shadow
+          position = "bottom", -- bottom, top
+          margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+          padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
+          winblend = 0,
+        },
+        layout = {
+          height = { min = 4, max = 25 }, -- min and max height of the columns
+          width = { min = 20, max = 50 }, -- min and max width of the columns
+          spacing = 3, -- spacing between columns
+          align = "left", -- align columns left, center or right
+        },
+      },
+      defaults = {
+        mode = { "n", "v" },
+        ["<leader>f"] = { name = "+File" },
+        ["<leader>q"] = { name = "+Quit/Session" },
+        ["<leader>qq"] = { cmd = "<cmd>q<cr>", desc = "Quit" },
+        ["<leader>w"] = { cmd = "<cmd>update!<cr>", desc = "Save" },
+      },
+    },
+    config = function(_, opts)
+      local wk = require "which-key"
+      wk.setup(opts.setup)
+      wk.register(opts.defaults)
+    end,
+  },
+}
---
+++ lua/base/ui.lua
@@ -0,0 +1,75 @@
+return {
+  {
+    "nvim-tree/nvim-web-devicons",
+    dependencies = { "DaikyXendo/nvim-material-icon" },
+    config = function()
+      require("nvim-web-devicons").setup {
+        override = require("nvim-material-icon").get_icons(),
+      }
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+      transparent = false,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+    config = function(_, opts)
+      local tokyonight = require "tokyonight"
+      tokyonight.setup(opts)
+      tokyonight.load()
+    end,
+  },
+  { "catppuccin/nvim", lazy = false, name = "catppuccin" },
+  {
+    "stevearc/dressing.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    event = "VeryLazy",
+    enabled = true,
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            find = "%d+L, %d+B",
+          },
+          view = "mini",
+        },
+      },
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = true, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+    },
+    --stylua: ignore
+    keys = {
+      { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+      { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward" },
+      { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward"},
+    },
+  },
+}
